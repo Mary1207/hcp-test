4a22d85096aa77c5f227b38643845842169cb735
Mary1207 <mary.george.gd@gmail.com>
2025-05-03 09:35:56 -0400

    update2

--- provider.tf
+++ provider.tf
@@ -1,4 +1,8 @@
-provider "aws" {
-  region  = "us-east-1"
-  # profile = "default"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.97.0"
+    }
+  }
 }
