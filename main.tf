a4a128a0b8c3eb997a232de7abc64df0931bb3b7
Mary1207 <mary.george.gd@gmail.com>
2025-05-07 05:38:44 -0400

    off

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "test" {
-  ami = "ami-0f88e80871fd81e91"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "steven"
-  }
-}
+# resource "aws_instance" "test" {
+#   ami = "ami-0f88e80871fd81e91"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "steven"
+#   }
+# }
 
 resource "aws_iam_user" "tivo" {
   name = "loadbalancer"
